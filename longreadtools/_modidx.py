e650c79da41383346ae12c626378faff55f17373
Eamon <mcandrew@bego.ipmc.cnrs.fr>
2024-01-31 15:01:14 +0100

    Added Isomatrix tools module and the isomatrix to anndat function with tests

--- longreadtools/_modidx.py
+++ longreadtools/_modidx.py
@@ -6,5 +6,9 @@ d = { 'settings': { 'branch': 'main',
                 'git_url': 'https://github.com/cobioda/longreadtools',
                 'lib_path': 'longreadtools'},
   'syms': { 'longreadtools.core': {'longreadtools.core.foo': ('tests.html#foo', 'longreadtools/core.py')},
-            'longreadtools.isomatrix_tools': { 'longreadtools.isomatrix_tools.isomatrix_to_anndata': ( 'isomatrix_tools.html#isomatrix_to_anndata',
-                                                                                                       'longreadtools/isomatrix_tools.py')}}}
+            'longreadtools.isomatrix_tools': { 'longreadtools.isomatrix_tools.download_test_data': ( 'isomatrix_tools.html#download_test_data',
+                                                                                                     'longreadtools/isomatrix_tools.py'),
+                                               'longreadtools.isomatrix_tools.isomatrix_to_anndata': ( 'isomatrix_tools.html#isomatrix_to_anndata',
+                                                                                                       'longreadtools/isomatrix_tools.py'),
+                                               'longreadtools.isomatrix_tools.test_isomatrix_to_anndata': ( 'isomatrix_tools.html#test_isomatrix_to_anndata',
+                                                                                                            'longreadtools/isomatrix_tools.py')}}}

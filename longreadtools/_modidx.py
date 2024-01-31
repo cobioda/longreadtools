0fe3a82221b66437e8344e1142c3b270a3350cc0
Eamon <mcandrew@bego.ipmc.cnrs.fr>
2024-01-31 16:44:23 +0100

    Added Isomatrix tools module and the isomatrix to anndata function with tests

--- longreadtools/_modidx.py
+++ longreadtools/_modidx.py
@@ -9,4 +9,6 @@ d = { 'settings': { 'branch': 'main',
             'longreadtools.isomatrix_tools': { 'longreadtools.isomatrix_tools.download_test_data': ( 'isomatrix_tools.html#download_test_data',
                                                                                                      'longreadtools/isomatrix_tools.py'),
                                                'longreadtools.isomatrix_tools.isomatrix_to_anndata': ( 'isomatrix_tools.html#isomatrix_to_anndata',
-                                                                                                       'longreadtools/isomatrix_tools.py')}}}
+                                                                                                       'longreadtools/isomatrix_tools.py'),
+                                               'longreadtools.isomatrix_tools.multiple_isomatrix_conversion': ( 'isomatrix_tools.html#multiple_isomatrix_conversion',
+                                                                                                                'longreadtools/isomatrix_tools.py')}}}
